0c34167c2582688a4355519170ee160f8c59a79c
Natalia <106815965+NataCalengil@users.noreply.github.com>
2023-10-06 17:17:27 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -2,5 +2,6 @@ const items = document.querySelectorAll('.item');
 Array.prototype.forEach.call(items, (element) => {
     const title = element.querySelector('h2').innerHTML;
     const itemsLength = element.querySelectorAll('li').length;
-    console.log(`Categories: ${title} / Elements: ${itemsLength}`);
+    console.log(`Category: ${title}`);
+    console.log(`Elements: ${itemsLength}`);
 });

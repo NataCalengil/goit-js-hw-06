88f5ecfbf0a0a49f8889f0aef8f3c6b892112780
Natalia <106815965+NataCalengil@users.noreply.github.com>
2023-10-19 22:36:04 +0300

    update

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(loginForm);
+
+  const userData = {};
+
+  formData.forEach((value, key) => {
+    userData[key] = value;
+  });
+
+  if (userData.email.trim() === '' || userData.password.trim() === '') {
+    alert('Перевірте заповненість полів');
+  } else {
+    console.log(userData);
+    loginForm.reset();
+  }
+});
